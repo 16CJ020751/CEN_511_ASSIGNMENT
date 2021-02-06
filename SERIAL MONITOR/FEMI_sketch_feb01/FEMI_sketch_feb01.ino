e3bc4406f3077b099b9683d3321bcd3b4d35569a
16CJ020751 <78654876+16CJ020751@users.noreply.github.com>
2021-02-06 18:10:05 +0100

    Add files via upload

---
+++ SERIAL MONITOR/FEMI_sketch_feb01/FEMI_sketch_feb01.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello My name is OLUFEMI OJEYEMI, 16CJ020751");
+  delay(3000);
+}
